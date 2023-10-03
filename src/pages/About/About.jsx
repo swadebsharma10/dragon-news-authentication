984490df28d03c06334949c99ca1f70db5a09b65
swadebsharma6 <swadebriva2012@gmail.com>
2023-10-04 01:42:21 +0600

    added about and career

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,20 @@
+import Header from "../../SharedPages/Header/Header";
 
 
 const About = () => {
     return (
-        <div>
-         <h3>About page</h3>   
+        <section>
+        <Header></Header>
+        <div className="h-[80vh] flex justify-center items-center">
+         <div className="text-center">
+         <h3 className="text-3xl text-primary underline">About</h3> 
+         <h2 className="text-2xl font-bold text-purple-500">Dragon News.Org</h2> 
+         <p>Author: Dr. Dragon </p> 
+         <p>Email: dragonnews@Gmail.com</p>
+         </div>
+
         </div>
+        </section>
     );
 };
 
--- src/pages/Career/Career.jsx
+++ src/pages/Career/Career.jsx
@@ -1,10 +1,20 @@
+import Header from "../../SharedPages/Header/Header";
 
 
 const Career = () => {
     return (
-        <div>
-            <h3>Career</h3>
+        <section>
+        <Header></Header>
+        <div className="h-[80vh] flex justify-center items-center">
+         <div className="text-center">
+         <h3 className="text-3xl text-primary underline">Career</h3> 
+         <h2 className="text-2xl font-bold text-purple-500">Dragon News.Org</h2> 
+         <p>Author: Dr. Dragon </p> 
+         <p>Email: dragonnews@Gmail.com</p>
+         </div>
+
         </div>
+        </section>
     );
 };
 
